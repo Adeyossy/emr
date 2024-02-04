23ae1853da9e4bf8ba1d67c86191559a9c4c4956
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-02-05 00:50:08 +0100

    fix: Remove the card background around a form group

--- src/components/forms/form_group.js
+++ src/components/forms/form_group.js
@@ -4,7 +4,7 @@ import FormItemComponent from "./form_item";
 export default class FormGroupComponent extends React.Component {
   render() {
     return (
-      <div className="emr-clerking-tab-data-item">
+      <div className="pt-4">
         <h6 className="emr-card-headers">{this.props.groupName}</h6>
         <div className="emr-clerking-tab-data-items">
           {
