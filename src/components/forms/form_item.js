a191472f0f2ff7091352d2585f06d042d79d7729
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-02-05 09:33:54 +0100

    fix: Base selected item on index, remove ul

--- src/components/forms/form_item.js
+++ src/components/forms/form_item.js
@@ -8,9 +8,9 @@ export default class FormItemComponent extends React.Component {
   update = (index) => {
     const thisForm = this.context[this.context.last_viewed].forms[this.props.formTag];
     const value = thisForm[this.props.field];
-    const found = this.props.desc.find((_des, i) => i === index);
-    if (found) this.props.onItemChange(this.props.field, this.props.min);
-    else this.props.onItemChange(this.props.field, index);
+    const found = this.props.desc.findIndex(des => des === value);
+    if (found === index) this.props.onItemChange(this.props.field, this.props.min - 1);
+    else this.props.onItemChange(this.props.field, index + this.props.min);
   }
 
   render() {
@@ -21,12 +21,8 @@ export default class FormItemComponent extends React.Component {
         <div className="row">
           <div className="col-9">
             <div className="emr-form-item">
-              <ul>
-                {
-                  <SingleItemSelectComponent selectableItems={this.props.desc} 
-                    selectedItem={this.props.desc[value]} displayInBox={this.update} />
-                }
-              </ul>
+              <SingleItemSelectComponent selectableItems={this.props.desc}
+                selectedItem={this.props.desc[value - this.props.min]} displayInBox={this.update} />
             </div>
           </div>
           {
@@ -34,7 +30,7 @@ export default class FormItemComponent extends React.Component {
               <div className="col-3">
                 <div className="emr-form-item-score">
                   <input type="number" name={this.props.field} min={this.props.min}
-                    max={this.props.max} onChange={(event) => this.props.onItemChange(event.target.name, 
+                    max={this.props.max} onChange={(event) => this.props.onItemChange(event.target.name,
                       event.target.value)}
                     value={value}>
                   </input>
