c6b55261c73a728794202abf33d0f70e22e85240
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-02-05 10:32:15 +0100

    feat: Add headers to items

--- src/models/rosier.js
+++ src/models/rosier.js
@@ -1,27 +1,27 @@
 const rosier = [
   [
-    ["Unilateral Facial Weakness (+1)"], "facialweakness", 0, 1
+    ["No", "Yes"], "facialweakness", 0, 1, "Unilateral Facial Weakness (+1)"
   ],
   [
-    ["Unilateral Grip Weakness (+1)"], "gripweakness", 0, 1
+    ["No", "Yes"], "gripweakness", 0, 1, "Unilateral Grip Weakness (+1)"
   ],
   [
-    ["Unilateral Arm Weakness (+1)"], "armweakness", 0, 1
+    ["No", "Yes"], "armweakness", 0, 1, "Unilateral Arm Weakness (+1)"
   ],
   [
-    ["Unilateral Leg Weakness (+1)"], "legweakness", 0, 1
+    ["No", "Yes"], "legweakness", 0, 1, "Unilateral Leg Weakness (+1)"
   ],
   [
-    ["Speech Loss (+1)"], "speechloss", 0, 1
+    ["No", "Yes"], "speechloss", 0, 1, "Speech Loss (+1)"
   ],
   [
-    ["Visual Field Defect (+1)"], "visualfielddefect", 0, 1
+    ["No", "Yes"], "visualfielddefect", 0, 1, "Visual Field Defect (+1)"
   ],
   [
-    ["Loss of Consciousness (-1)"], "lossofconsciousness", -1, 0
+    ["Loss of Consciousness (-1)", "No LOC"], "lossofconsciousness", -1, 0, "Consciousness"
   ],
   [
-    ["Seizure (-1)"], "seizure", -1, 0
+    ["Present (-1)", "Absent (0)"], "seizure", -1, 0, "Seizure"
   ],
 ];
 
