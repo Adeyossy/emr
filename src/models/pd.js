5bef37f7c802cd02713de6188d34b3d8ad2c5f2b
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-02-06 09:03:55 +0100

    feat: Create parkinson disease items

---
+++ src/models/pd.js
@@ -0,0 +1,39 @@
+const featuresItems = [
+  [
+    [
+      "Yes (0)", "Uncertain (1)", "Uncertain (2)"
+    ], "bradykinesia", 0, 3, "Bradykinesia is present and typical for parkinsonism",
+    `Slowness of movement and decrement in amplitude or speed or progressive hesitations/halts 
+    as movements are continued`
+  ],
+  [
+    [
+      "Yes (0)", "Uncertain (1)", "Uncertain (2)"
+    ], "rigidity", 0, 2, "Rigidity is present and typical for parkinsonism", 
+    `Judged on slow passive movements of major joints with the patient in the relaxed position. 
+    Rigidity refers to 'lead-pipe' resistance that is velocity-independence resistance to 
+    passive movement not solely reflecting failure to relax. Isolated cogwheeling without 
+    lead-pipe rigidity does not fulfill criteria for rigidity`
+  ],
+  [
+    [
+      "Yes (0)", "Uncertain (1)", "Uncertain (2)"
+    ], 
+    "resttremor", 0, 2, "Rest tremor is present and typical for parkinsonism",
+    `Rest tremor refers to 4-6 Hz tremor in the fully resting limb which is 
+    suppressed during movement initiation. Kinetic and postural tremor do not 
+    qualify for parkinsonism criteria.`
+  ],
+  [
+    [
+      "Yes (0)", "Uncertain (1)", "Uncertain (2)"
+    ], 
+    "posturaldisturbance", 0, 2, "Postural disturbance is present and typical for parkinsonism",
+    `Postural instability not caused by primary visual, vestibular, cerebellar or 
+    proprioceptive dysfunction`
+  ]
+];
+
+export const pdItems = [
+  ["Features of Parkinson", featuresItems]
+];
