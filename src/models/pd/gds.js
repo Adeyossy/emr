c2dbc3811875f231c186c384a4a5283ff6b578a3
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-03-13 18:09:40 +0100

    feat: Add 7 items in geriatric depression scale-15

---
+++ src/models/pd/gds.js
@@ -0,0 +1,67 @@
+const options = ["No (0)", "Yes (1)", "N/A (2)"];
+
+const gdsItems = [
+  [
+    ["No (0)", "Yes (1)", "N/A (2)"],
+    "basicallysatisfied",
+    0,
+    2,
+    "Are you basically satisfied with your life?",
+    "",
+    ""
+  ],
+  [
+    ["No (0)", "Yes (1)", "N/A (2)"],
+    "droppedactivitiesandinterests",
+    0,
+    2,
+    "Have you dropped many of your activities and interests?",
+    "",
+    ""
+  ],
+  [
+    ["No (0)", "Yes (1)", "N/A (2)"],
+    "feellifeempty",
+    0,
+    2,
+    "Do you feel that your life is empty?",
+    "",
+    ""
+  ],
+  [
+    ["No (0)", "Yes (1)", "N/A (2)"],
+    "oftengetbored",
+    0,
+    2,
+    "Do you often get bored?",
+    "",
+    ""
+  ],
+  [
+    ["No (0)", "Yes (1)", "N/A (2)"],
+    "goodspirits",
+    0,
+    2,
+    "Are you in good spirits most of the time?",
+    "",
+    ""
+  ],
+  [
+    ["No (0)", "Yes (1)", "N/A (2)"],
+    "afraidsomethingbad",
+    0,
+    2,
+    "Are you afraid that something bad is going to happen to you?",
+    "",
+    ""
+  ],
+  [
+    options,
+    "feelhappymosttime",
+    0,
+    2,
+    "Do you feel happy most of the time?",
+    "",
+    ""
+  ]
+]
