f3a0e276107feb255a7ae679b8907c3bae3589e5
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-04-05 16:42:26 +0100

    feat: Add mds form

--- src/models/pd.js
+++ src/models/pd.js
@@ -169,6 +169,114 @@ const responseToTherapyItems = [
   ]
 ]
 
+export const mdsItems = [
+  [
+    ["Present (0)", "Absent (1)"],
+    "parkinsonism",
+    0,
+    1,
+    "Parkinsonism",
+    "",
+    ""
+  ],
+  [
+    ["Bradykinesia (0)", "Rest tremor (1)", "Rigidity (2)"],
+    "criteria",
+    0,
+    2,
+    "Criteria (tick all that apply)",
+    "",
+    ""
+  ],
+  [
+    ["Clinically Established PD (0)", "Clinically Probable PD (1)"],
+    "diagnosticcategory",
+    0,
+    1,
+    "Diagnostic category (see criteria below category)",
+    "",
+    ""
+  ],
+  [
+    [
+      "Clear and dramatic beneficial response to dopaminergic therapy (1)", 
+      "Presence of levodopa-induced dyskinesia (2)",
+      "Rest tremor of a limb, documented on clinical examination (in past, or on current examination) (3)",
+      "The presence of either olfactory loss (or cardiac sympathetic denervation on MIBG Scintigraphy) (4)"
+    ],
+    "supportivecriteria",
+    1,
+    4,
+    "Supportive criteria (Check box if criteria met)",
+    "",
+    ""
+  ],
+  [
+    [
+      `Unequivocal cerebellar abnormalities such as cerebellar gait, limb ataxia, or cerebellar 
+       oculomotor abnormalities (e.g. sustained gaze evoked nystagmus, macro square wave jerks,
+        hypermetric saccades) (1)`,
+      `Downward vertical supranuclear gaze palsy, or selective slowing of downward vertical 
+      saccades (2)`,
+      `Diagnosis of probable behavioural variant frontotemporal dementia or primary progressive 
+      aphasia, defined according to consensus criteria within the first 5 years of disease (3)`,
+      `Parkinsonian features restricted to the lower limbs for more than 3 years (4)`,
+      `Treatment with a dopamine receptor blocker or a dopamine-depleting agent in a dose and 
+      time-course consistent with drug-induced parkinsonism (5)`,
+      `Absence of observable response to high-dose levodopa despite at least moderate severity 
+      of disease (6)`,
+      `Unequivocal cortical sensory loss (i.e. graphaesthesia, stereognosis with intact primary 
+      sensory modalities), clear limb ideomotor apraxia, or progressive aphasia (7)`,
+      `Normal functional neuroimaging of the presynaptic dopaminergic system (8)`,
+      `Documentation of an alternative condition known to produce parkinsonism and plausibly 
+      connected to the patient's symptoms, or, the expert evaluating physician, based on the 
+      full diagnostic assessment feels that an alternative syndrome is more likely than PD (9)`
+    ],
+    "absoluteexclusioncriteria",
+    1,
+    9,
+    "Absolute exclusion criteria",
+    "",
+    ""
+  ],
+  [
+    [
+      `Rapid progression of gait impairment requiring regular use of wheelchair within 5 years 
+      of onset (1)`,
+      `A complete absence of progression of motor symptoms or signs over 5 or more years unless 
+      stability is related to treatment (2)`,
+      `Early bulbar dysfunction: severe dysphonia or dysarthria (speech unintelligble most of 
+      the time) or severe dysphagia (requiring soft food, NG tube, or gastrostomy feeding) 
+      within first 5 years (3)`,
+      `Inspiratory respiratory dysfunction: either diurnal or nocturnal inspiratory stridor or
+      frequent inspiratory sighs (4)`,
+      `Severe autonomic failure in the first 5 years of disease. This can include: Orthostatic 
+      hypotension or severe urinary retention or urinary incontinence in the first 5 years of 
+      disease that is not simply functional incontinence (5)`,
+      `Recurrent (>1/y) falls because of impaired balance within 3 years of onset (6)`,
+      `Disproportionate anterocollis (dystonic) or contractures of hand or feet within the 
+      first 10 years (7)`,
+      `Absence of any of the common nonmotor features of disease despite 5 years disease duration.
+      These include sleep dysfunction (sleep-maintenance insomnia, excessive daytime somnolence, 
+      symptoms of REM sleep behaviour disorder), autonomic dysfunction (constipation, daytime 
+      urinary urgency, symptomatic orthostasis), hyposmia, or psychiatric dysfunction 
+      (depression, anxiety, or hallucinations) (8)`,
+      `Otherwise-unexplained pyramidal tract signs, defined as pyramidal weakness or clear 
+      pathologic hyperreflexia (excluding mild reflex asymmetry and isolated extensor plantar 
+      response) (9)`,
+      `Bilateral symmetric parkinsonism. The patient or caregiver reports bilateral symptom 
+      onset with no side predominance, and no side predominance is observed on objective 
+      examination (10)`
+    ],
+    "redflags",
+    1,
+    10,
+    "Red flags",
+    "",
+    ""
+  ]
+]
+
 export const pdItems = [
   ["Features of Parkinson", featuresItems],
   ["Other features supportive of Parkinson's disease", otherFeatures],
